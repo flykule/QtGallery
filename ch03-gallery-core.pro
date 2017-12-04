f2f02dd3f562dd2b08a24a219eef1b04107b4fca
castle <castleliu40@gmail.com>
2017-12-04 11:31:30 +0800

    add mobile project

--- ch03-gallery-core.pro
+++ ch03-gallery-core.pro
@@ -2,7 +2,9 @@ TEMPLATE = subdirs
 
 SUBDIRS += \
     gallery-core \
-    gallery-desktop
+    gallery-desktop \
+    gallery-mobile
 # depends attribute helps qmake process project in parallet when possible
 gallery-desktop.depends = gallery-core
+#gallery-mobile.depends = gallery-core
 
---
+++ gallery-mobile/main.cpp
@@ -0,0 +1,26 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QQuickView>
+
+#include "albummodel.h"
+#include "picturemodel.h"
+
+int main(int argc, char *argv[]) {
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QGuiApplication app(argc, argv);
+
+  AlbumModel albumModel;
+  PictureModel pictureModel(albumModel);
+
+  QQmlApplicationEngine engine;
+  QQmlContext *context = engine.rootContext();
+  context->setContextProperty("albumModel", &albumModel);
+  context->setContextProperty("pictureModel", &pictureModel);
+
+  engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+  if (engine.rootObjects().isEmpty())
+    return -1;
+
+  return app.exec();
+}
